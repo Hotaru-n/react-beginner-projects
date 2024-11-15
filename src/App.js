980c287b69f5a932218287e28cdbb4500bf647e0
Hotaru Void <desuipd@gmail.com>
2024-11-15 20:05:52 +0300

    done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import { Users } from "./components/Users";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     fetch("https://reqres.in/api/users")
@@ -22,9 +25,38 @@ function App() {
       .finally(() => setLoading(false));
   }, []);
 
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  };
+
   return (
     <div className="App">
-      <Users items={users} isLoading={isLoading} />
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
+
       {/* <Success /> */}
     </div>
   );
